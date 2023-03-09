dee6b6d91eb0245fce12d5958549b863ce1b5a43
CHEN211410674 <211410674@o365.tku.edu.tw>
2023-03-09 19:59:16 +0800

    W04-P2: show all data in array menu into html (section-center)

--- menu_starter_74/app_74.js
+++ menu_starter_74/app_74.js
@@ -1,6 +1,6 @@
 const menu = [
     {
-    id: 1;
+    id: 1,
     title: 'yogurt',
     category: 'breakfast',
     price: 9.9,
@@ -9,16 +9,16 @@ const menu = [
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, odit.'
     },
     {
-    id: 2;
+    id: 2,
     title: 'pasta',
     category: 'lunch',
     price: 13.99,
-    img: './images/item-2.jpeg',
+    img: './images/lunch.jpg',
     remote_img: '',
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, odit.'
     },
     {
-    id: 3;
+    id: 3,
     title: 'vegetable',
     category: 'dinner',
     price: 15.99,
@@ -27,7 +27,7 @@ const menu = [
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, odit.'
     },
     {
-    id: 4;
+    id: 4,
     title: 'buttermilk pancakes',
     category: 'breakfast',
     price: 15.59,
@@ -36,7 +36,7 @@ const menu = [
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, odit.'
     },
     {
-    id: 5;
+    id: 5,
     title: 'dinner double',
     category: 'dinner',
     price: 13.99,
@@ -45,7 +45,7 @@ const menu = [
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, odit.'
     },
     {
-    id: 6;
+    id: 6,
     title: 'godzilla milkshake',
     category: 'shake',
     price: 13.99,
@@ -58,4 +58,31 @@ const menu = [
 
 const sectionCenter = document.querySelector('.section-center');
 const btnContainer = document.querySelector('btn-container');
+
+const displayMenuItems = (menu) => {
+    let displayMenu = menu.map((item) =>{
+      return`
+      <article class="menu-item">
+      <img src=${item.img} alt=${item.title} class="photo" />
+      <div class="item-info">
+        <header>
+          <h4>${item.title}</h4>
+          <h4 class="price">${item.price}</h4>
+        </header>
+        <p class="item-text">
+          ${item.desc}
+        </p>
+      </div>
+    </article>
+      `
+    });
+    console.log('displayMenu', displayMenu);
+    displayMenu = displayMenu.join('');
+    console.log('displayMenu after join', displayMenu);
+    sectionCenter.innerHTML = displayMenu;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    displayMenuItems(menu);
+});
 
