29ed3682e0b56a49b3ab57609ec37408ac663646
CHEN211410674 <211410674@o365.tku.edu.tw>
2023-03-09 19:20:52 +0800

    add 3 images of my meals to menu array

---
+++ menu_starter_74/app_74.js
@@ -0,0 +1,61 @@
+const menu = [
+    {
+    id: 1;
+    title: 'yogurt',
+    category: 'breakfast',
+    price: 9.9,
+    img: './images/yogurt.jpg',
+    remote_img: '',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, odit.'
+    },
+    {
+    id: 2;
+    title: 'pasta',
+    category: 'lunch',
+    price: 13.99,
+    img: './images/item-2.jpeg',
+    remote_img: '',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, odit.'
+    },
+    {
+    id: 3;
+    title: 'vegetable',
+    category: 'dinner',
+    price: 15.99,
+    img: './images/dinner.jpg',
+    remote_img: '',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, odit.'
+    },
+    {
+    id: 4;
+    title: 'buttermilk pancakes',
+    category: 'breakfast',
+    price: 15.59,
+    img: './images/item-1.jpeg',
+    remote_img: '',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, odit.'
+    },
+    {
+    id: 5;
+    title: 'dinner double',
+    category: 'dinner',
+    price: 13.99,
+    img: './images/item-2.jpeg',
+    remote_img: '',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, odit.'
+    },
+    {
+    id: 6;
+    title: 'godzilla milkshake',
+    category: 'shake',
+    price: 13.99,
+    img: './images/item-3.jpeg',
+    remote_img: '',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, odit.'
+    },
+    
+];
+
+const sectionCenter = document.querySelector('.section-center');
+const btnContainer = document.querySelector('btn-container');
+
