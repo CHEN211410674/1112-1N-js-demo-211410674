845899cd5940773ff59186d6e6f2d90764aa5f71
CHEN211410674 <211410674@o365.tku.edu.tw>
2023-03-16 21:56:37 +0800

    w05

--- demo/w05-modal-starter/script_74.js
+++ demo/w05-modal-starter/script_74.js
@@ -1,23 +1,32 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
+const modal = document.querySelectorAll('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelectorAll('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log('modal', modal);
 console.log('show-modal', btnsOpenModal);
 
-const openModal = () => {
-    modal.classList.remove('hidden');
+const openModal = (index) => {
+    modal[index].classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = (index) => {
+    modal[index].classList.add('hidden');
     overlay.classList.remove('hidden');
 }
 
 for(let i=0; i<btnsOpenModal.length; i++){
-    btnsOpenModal[i].addEventListener('click', openModal(i));
-}
+    btnsOpenModal[i].addEventListener('click', (e) => openModal(i))
+    btnsCloseModal[i].addEventListener('click', (e) => closeModal(i))
 
-document.addEventListener('keydon', (e) =>{
-    if (e.key === 'ESCAPE' && !modal.classList.contains('hidden')){
-        closeModal();
-    }
-});
+    document.addEventListener('keydon', (e) =>{
+        if (e.key === 'Escape' && !modal[i].classList.contains('hidden')){
+            closeModal(i);
+        }
+    });
+};
+
+
+
