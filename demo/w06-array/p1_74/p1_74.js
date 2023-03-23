f3706775a98da409148995fd453e15f4d93c03c0
CHEN211410674 <211410674@o365.tku.edu.tw>
2023-03-23 20:32:45 +0800

    ### W06-P3: import students and sdata array and do sorting, find the highest and lowest score~

--- demo/w06-array/p1_74/p1_74.js
+++ demo/w06-array/p1_74/p1_74.js
@@ -1,6 +1,6 @@
 let c, f;
 
-c = Number(prompt("Enter a temperature in C:")).toFixed(2);
+c = Number(prompt("Enter a temperature in C:")).toFixed(2);//取小數點後兩位
 
 f = (c *9 /5 + 32).toFixed(2);
 
---
+++ demo/w06-array/p2_74/data_74.js
@@ -0,0 +1,29 @@
+export const students = [
+    {
+        id : 1,
+        name :'Chen Yi-Zhen',
+        score :80
+    },
+    {
+        id : 2,
+        name :'Luciel',
+        score :90
+    },
+    {
+        id :3,
+        name :'Zen',
+        score : 50
+    },
+    {
+        id :4,
+        name :'peter',
+        score :60
+    },
+    {
+        id :5,
+        name :'jane',
+        score: 20
+    },
+];
+
+export const sdata = [90, 100, 60, 40, 20, 80];
