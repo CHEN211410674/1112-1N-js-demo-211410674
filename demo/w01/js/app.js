4e2695e0b74d4321d8d4962f59d2598cd77a7285
CHEN211410674 <211410674@o365.tku.edu.tw>
2023-04-09 19:00:01 +0800

    添加w01註解

--- demo/w01/js/app.js
+++ demo/w01/js/app.js
@@ -1,8 +1,8 @@
-const userInput = document.querySelector('#input-number');
-const addBtn = document.querySelector('#btn-add');
-const subtract = document.querySelector('#btn-subtract');
-const multiply = document.querySelector('#btn-multiply');
-const divide = document.querySelector('#btn-divide');
+const userInput = document.querySelector('#input-number'); //透過 const 可以宣告常數
+const addBtn = document.querySelector('#btn-add'); //querySelector:根據傳入的值來對應到指定的元素
+const subtract = document.querySelector('#btn-subtract'); //querySelector:屬於CSS選擇器，屬於靜態結果
+const multiply = document.querySelector('#btn-multiply'); //Selector選擇器
+const divide = document.querySelector('#btn-divide'); //html裡的id要加#
 
 const currentResultOutput = document.querySelector('#current-result');
 const currentCalculationOutput = document.querySelector('#current-calculation');
@@ -16,27 +16,30 @@ const currentCalculationOutput = document.querySelector('#current-calculation');
 //console.log('currentResultOutput', currentResultOutput);
 //console.log('currentCalculationOutput', currentCalculationOutput);
 
-const defaultResult = 0;
-let currentResult = defaultResult;
+const defaultResult = 0; //const宣告常數，在被宣告時就務必要指定給值，不然會產生錯誤。
+let currentResult = defaultResult; //let宣告變數
 
-
+//顯示使用者輸入的數字
 function getUserInput() {
-    return parseInt(userInput.value); //字串轉換成數字
-}
+    return parseInt(userInput.value); // parseInt:字串轉換成數字
+} //一個函數可以有返回值，使用 return 關鍵字來返回值
 
-function outputResult(result, text) {
+
+//顯示operand1 + operand2 and Result
+
+function outputResult(result, text) { //小括號裡面指定這個函數的參數
     currentResultOutput.textContent = result; 
     currentCalculationOutput.textContent = text;
-}
+} //一個 function 也可以沒有返回值，省略 return ，預設會返回 undefined
 
 // operand1 operator operand2 0 + 5
 function add() {
-    const operand1 = currentResult;
-    const operand2 = getUserInput();
+    const operand1 = currentResult; // currentResult :最初定義常數值為0, ...
+    const operand2 = getUserInput(); // getUserInput() :輸入的數字
     currentResult = operand1 + operand2;
-    console.log(`${operand1} + ${operand2} = ${currentResult}` );
-    const calcText = `${operand1} + ${operand2}`;
-    outputResult(currentResult, calcText);
+    console.log(`${operand1} + ${operand2} = ${currentResult}` ); //使用反引號(``)和${}，可以將變數包在整句字串內。
+    const calcText = `${operand1} + ${operand2}`; //顯示於Result:
+    outputResult(currentResult, calcText); //呼叫函式
 }
 
 
@@ -72,8 +75,8 @@ function divi() {
 
 
 
-addBtn.addEventListener('click', add);
-subtract.addEventListener('click', sub);
-multiply.addEventListener('click', mult);
-divide.addEventListener('click', divi);
+addBtn.addEventListener('click', add); //當使用者點擊+時，執行add函式
+subtract.addEventListener('click', sub); //當使用者點擊-時，執行sub函式
+multiply.addEventListener('click', mult); //當使用者點擊*時，執行mult函式
+divide.addEventListener('click', divi); //當使用者點擊/時，執行divi函式
 
