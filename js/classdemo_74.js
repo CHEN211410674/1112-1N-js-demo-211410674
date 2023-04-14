c228b41bd5dada40a486759905fb706117204e93
CHEN211410674 <211410674@o365.tku.edu.tw>
2023-04-14 11:03:03 +0800

    W09-P5: Mega Menu deploy to Vercel

--- js/classdemo_74.js
+++ js/classdemo_74.js
@@ -1,10 +1,10 @@
 const showClassDemo = (week) => {
     const showDemo = document.querySelector('.banner-section');
     switch(week){
-        case 'w1';
-          showDemo.innerHTML = `<iframe src='./demo/w01/index.html' width="100%" height="100vh"/>`;
+        case 'w1':
+          showDemo.innerHTML = `<iframe src='../demo/w01/index.html' width="100%" height="100%"/>`;
           break;
-        case 'w2';
+        case 'w2':
         
          break;
    }
