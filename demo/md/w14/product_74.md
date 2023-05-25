0be70325441e5c61fa76259fcb1cf723d300cb04
CHEN211410674 <211410674@o365.tku.edu.tw>
2023-05-25 19:10:17 +0800

    W15-P1: prepare menu items for API project in Vercel

---
+++ demo/md/w14/product_74.md
@@ -0,0 +1,9 @@
+### [Github](https://github.com/CHEN211410674/1112-1N-js-demo-211410674.git)
+
+### [vercel](https://1112-1-n-js-demo-211410674.vercel.app/)
+
+### W14-P3: Get products from url
+
+![w14-p3.png](https://sgtwgxsjtbibcbrzrfra.supabase.co/storage/v1/object/public/demo-74/w14_74/w14-p3.png)
+
+![w14-p3-1.png](https://sgtwgxsjtbibcbrzrfra.supabase.co/storage/v1/object/public/demo-74/w14_74/w14-p3-1.png)
---
+++ demo/w15-store-product/js/app_74.js
@@ -0,0 +1,40 @@
+const url = `https://course-api.com/javascript-store-products`;
+
+let products;
+
+const fetchData = async ()=>{
+    try {
+        const response = await fetch(url);
+        const data = await response.json();;
+        console.log('data', data);
+        return data;
+    //   displayItems(data.meals);
+    } catch (error) {
+        console.error(error);
+    }  
+}
+
+const displayItems = (items) => {
+  const displayData = items
+    .map((item) => {
+      const { name, image , price} = item.fields;
+      return `
+      <div class="single-product">
+      <img src=${image[0].url} class="single-product-img img" alt="albany table">
+      <footer>
+        <h5 class="name">${name}</h5>
+        <span class="price">${price}</span>
+      </footer>
+    </div>
+      `;
+    })
+    .join('');
+   const container = document.querySelector('.products-container')
+  container.innerHTML = displayData;
+};
+
+
+window.addEventListener('DOMContentLoaded', async () => {
+    products = await fetchData();
+    await displayItems(products);
+});
