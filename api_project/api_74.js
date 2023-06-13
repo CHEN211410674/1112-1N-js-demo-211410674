832f52719cb1957aac80f57445f5498a740e70bc
CHEN211410674 <211410674@o365.tku.edu.tw>
2023-06-13 22:38:24 +0800

    連上Api

--- api_project/api_74.js
+++ api_project/api_74.js
@@ -1,110 +1,17 @@
-const search = document.querySelector('#search');
-const submit = document.querySelector('#submit');
-const random = document.querySelector('#random');
-const resultHeading = document.querySelector('#result-heading');
-const mealsE1 = document.querySelector('#meals');
-const single_mealE1 = document.querySelector('#single-meal');
+// const url = 'https://chinese-food-db.p.rapidapi.com/';
 
-const searchMeal = (e) => {
-   e.preventDefault();
-   const term = search.value;
-   // console.log('term', term);
-   if(term.trim()){
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
-      .then(response => response.json())
-      .then(data =>{
-        console.log('data', data);
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'be29625424mshd7539069170c2fdp1f06d9jsn8b03c9a54c32',
+		'X-RapidAPI-Host': 'chinese-food-db.p.rapidapi.com'
+	},
+};
 
-        if(data.meals === null){
-            resultHeading.innerHTML = `<p> There are no search results. Try again! </p>`
-        } else {
-            resultHeading.innerHTML = `<h2> Search Result for '${term}'</h2>`
-            mealsE1.innerHTML = data.meals.map((meal)=>{
-                return`
-                <div class="meal">
-                <img src="${meal.strMealThumb}" />
-                <div class="meal-info" data-mealID="${meal.idMeal}">
-                    <h3> ${meal.strMeal} </h3>
-                    </div>
-                </div> 
-                `
-            }).join('');
-        }
-    })
-} else {
-    //alert('Please enter a search term ');
-}
-}
-
-const getMealById = (mealID) =>{
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
-      .then(resp=>resp.json())
-      .then(data => {
-        console.log('single meal', data);
-        const meal = data.meals[0];
-        addMealToDOM(meal);
-    })
-}
-
-const addMealToDOM = (meal) =>{
-    const ingredients = [];
-    for(let i=1; i<=20; i++){
-        if(meal[`strIngredient${i}`]){
-            ingredients.push(`${meal[`strIngredient${i}`]}-${meal[`strMeasure${i}`]}`);
-        } else{
-            break;
-        }
-    }
-    console.log('ingredients', ingredients);
-
-    single_mealE1.innerHTML = `
-        <div class="single-meal">
-          <h1> ${meal.strMeal} </h1>
-          <img src="${meal.strMealThumb}">
-          <div class="main">
-            <p> ${meal.strInstructions} </p>
-            <h2> Ingredients </h2>
-            <ul>
-            ${ingredients.map(ing =>{
-                return `<li> ${ing} </li>`
-            }).join('')}
-            </ul>
-        </div>   
-      </div> 
-    `
-}
-
-//Event listeners
-submit.addEventListener('click', searchMeal);
-
-mealsE1.addEventListener('click', e => {
-    console.log('e.path', e.composedPath());
-    const composedPath = e.composedPath();
-    const mealInfo = composedPath.find(item =>{
-        if(item.classList){
-            return item.classList.contains('meal-info');
-        } else {
-            return false;
-        }
-    });
-    if(mealInfo) {
-        console.log('mealInfo', mealInfo);
-        const mealID = mealInfo.getAttribute('data-mealid');
-        getMealById(mealID);
-    }
-
-});
-
-//RandomBtn
-const randomBtn = () =>{
-    fetch(`https://www.themealdb.com/api/json/v1/1/random.php
-    `)
-      .then(resp=>resp.json())
-      .then(data => {
-        console.log('meal data', data);
-        const meal = data.meals[0];
-        addMealToDOM(meal);
-    })
-}
-
-random.addEventListener('click', randomBtn);
+fetch(
+	'https://chinese-food-db.p.rapidapi.com/',
+	options
+)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
