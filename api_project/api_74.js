29841ddeae4657c6d0d1f4b86beb8d5b87158b53
CHEN211410674 <211410674@o365.tku.edu.tw>
2023-06-14 21:09:35 +0800

    呈現出API裡的圖片

--- api_project/api_74.js
+++ api_project/api_74.js
@@ -1,4 +1,4 @@
-// const url = 'https://chinese-food-db.p.rapidapi.com/';
+const url = 'https://chinese-food-db.p.rapidapi.com/';
 
 const options = {
 	method: 'GET',
@@ -8,10 +8,50 @@ const options = {
 	},
 };
 
-fetch(
-	'https://chinese-food-db.p.rapidapi.com/',
-	options
-)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+// fetch(
+// 	'https://chinese-food-db.p.rapidapi.com/',
+// 	options
+// )
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+let allProducts;
+let products;
+
+const fetchData = async () => {
+  try{
+    const response = await fetch(`https://chinese-food-db.p.rapidapi.com/`, options);
+    const data = response.json();
+    console.log('fetch data', data);
+    return data;
+  }catch(err){
+    console.log(err);
+  }
+}
+
+	const displayItems = (items) => {
+		const displayData = items
+		  .map((item) => {
+			const { id, title, image } = item;
+			return `
+			<div class="single-product">
+			<img src=${image} class="single-product-img img" alt="albany table">
+			<footer>
+			  <h5 class="id">${id}</h5>
+			  <span class="title">${title}</span>
+			</footer>
+		  </div>
+			`;
+		  })
+		  .join('');
+		 const container = document.querySelector('.products-container')
+		container.innerHTML = displayData;
+	  };
+
+
+	  window.addEventListener('DOMContentLoaded', async () => {
+		products = await fetchData();
+		console.log('all products', allProducts);
+		await displayItems(products);
+	});
